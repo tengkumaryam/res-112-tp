b06c83481454932c733a3729c855489559c1ed44
tengkumaryam <tbintitengku-masrulhisham@boccard.fr>
2025-05-20 10:41:42 +0200

    build: inital commit for connection forwading

---
+++ forwarding_server_socket.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+"""A server module for the socket echo.
+"""
+
+import argparse
+import socket
+import signal
+
+#################################################
+## Parsing command line arguments
+parser = argparse.ArgumentParser(description='Echo Server')
+parser.add_argument('-p', '--port', dest='port', type=int, default=1800,
+                    metavar="[1024-49151]", 
+                    help='the port number to bind to')
+
+options = parser.parse_args()
+
+if options.port > 49151 or options.port < 1024:
+    print("Invalid port: the port number has to be between 1024 and 49151")
+    exit(1)
+
+
+#################################################
+## Global variables
+host = ''  # address to bind to. '' means all available interfaces
+s = None   # variable to store your socket
+
+
+#################################################
+## Handling of Ctrl+C
+def close_connection(sig, frame):
+    print('Ctrl+C Pressed. Closing the socket...')
+    if s:
+        # close the socket
+        s.close()
+    exit(0)
+
+# Register this handler for Ctrl+C (SIGINT) event
+signal.signal(signal.SIGINT, close_connection)
+
+
+
+
+#################################################
+## The main program
+
+# socket creation
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind with a local address and port
+s.bind((host, options.port))
+
+# make it a server socket
+s.listen(1)
+print("Server listening on port", options.port)
+
+# while True:
+#     # wait for a connection
+#     conn, address = s.accept()
+#     print("Connection accepted")
+#     while True:
+#         # receive the data
+#         data = conn.recv(1024)
+#         if not data:
+#             break
+#         print("Received: " + data.decode('utf-8'))
+#     print("Connection broken!")
+#     # we close the connection
+#     conn.close()
+
+conn1, address1 = s.accept()
+print("First client connected with adress ", address1)
+conn1.send(b"1")
+
+conn2, address2 = s.accept()
+print("First client connected with adress ", address2)
+conn1.send(b"2")
+
+try:
+    while True:
+        data = conn1.recv(1024)
+        if not data:
+            break
+        conn2.send(data)
+except:
+    pass
+
+print("Connection broken!")
+conn1.close()
+conn2.close()
